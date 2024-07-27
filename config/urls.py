d4aa176c2757c10cfdd3b2b26efd59c25a86a466
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-27 08:32:44 +0300

    fix urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('boaturls', namespace='boat')),
+    path('', include('boat.urls', namespace='boat')),
 
 ]
