c226cba564f73aedeb5a2a98d688f0d9510a5401
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-27 20:58:13 +0300

    добавил рассылку

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boat.urls', namespace='boat')),
+    path('order', include('order.urls', namespace='order')),
 
 ]
